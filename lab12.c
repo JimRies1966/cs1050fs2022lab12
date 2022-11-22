f74180c703f9e4b1c51e66a36da4baa7fe87eea1
Jim Ries <RiesJ@missouri.edu>
2022-11-22 12:04:57 -0600

    Added format string for fscanf

--- lab12.c
+++ lab12.c
@@ -14,4 +14,7 @@ int main(int argc, char * argv[])
 	//Combatant JimR={"JimR",21,900,10,"5d6+5","5d12+5"};
     //Combatant Lar={"Dr. Lawrence D. Ries",5,100,5,"1d6","N/A"};
 	//Fight(JimR,Lar);
+
+    // If you are going to use fscanf() to read data from the file, this format string might be helpful:
+    //" %[^,], %d, %d, %d, %[^,], %s"
 }
