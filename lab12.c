599a4384f0ca8c5a8b046f10a06296691299e0f3
Jim Ries <RiesJ@missouri.edu>
2022-11-22 11:43:08 -0600

    Starter for Lab 12

---
+++ lab12.c
@@ -0,0 +1,17 @@
+// Don't forget to add your header
+
+// Includes - do you need others?
+#include <stdio.h>
+#include "fight.h"
+
+// Main
+int main(int argc, char * argv[])
+{
+    // Uncomment the following line if you want random (different every time) fights
+    //SetSeed(0);
+
+    // Uncomment the following 3 lines if you want to try a sample fight
+	//Combatant JimR={"JimR",21,900,10,"5d6+5","5d12+5"};
+    //Combatant Lar={"Dr. Lawrence D. Ries",5,100,5,"1d6","N/A"};
+	//Fight(JimR,Lar);
+}
